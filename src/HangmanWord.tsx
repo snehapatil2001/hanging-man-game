8c0d7e134f69407e9d6bd486097fcb5e7e2c224f
Sneha <snehapatildeepali@gmail.com>
2024-02-06 14:27:22 +0530

    Feat: Display words which are typed on keyboard.

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -1,7 +1,30 @@
 export function HangmanWord() {
-  return (
-	<div>
-	  
-	</div>
-  )
+	const word = "test"
+	const guessedLetters = ["e"]
+	return (
+		<div
+			style={{
+				display: "flex",
+				gap: ".25em",
+				fontSize: "6rem",
+				fontWeight: "bold",
+				textTransform: "uppercase",
+				fontFamily: "monospace",
+			}}
+		>
+			{word.split("").map((letter, index) => (
+				<span style={{ borderBottom: ".1em solid black" }} key={index}>
+					<span
+						style={{
+							visibility: guessedLetters.includes(letter)
+								? "visible"
+								: "hidden",
+						}}
+					>
+						{letter}
+					</span>
+				</span>
+			))}
+		</div>
+	)
 }
